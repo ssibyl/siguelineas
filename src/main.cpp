dafedfce96496b2625a6fe1c25ddaff918a37f16
ssibyl <vairapaulina@gmail.com>
2023-12-17 10:43:26 -0300

    encendido y apagado de led por pulsador

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 #include <freertos/task.h>
 #include <driver/gpio.h>
 
-constexpr gpio_num_t LED_PIN = GPIO_NUM_2;
+constexpr gpio_num_t PULSADOR = GPIO_NUM_4;
+constexpr gpio_num_t LED_PIN = GPIO_NUM_15;
 
 extern "C" void app_main();
 
 void app_main() {
 
     // setup
-    gpio_config_t config;
+    gpio_config_t config = {};
 
     config.pin_bit_mask = (1<<LED_PIN);
     config.mode = GPIO_MODE_OUTPUT;
@@ -19,11 +20,19 @@ void app_main() {
 
     gpio_config(&config);
 
+    config.pin_bit_mask = (1<<PULSADOR);
+    config.mode = GPIO_MODE_INPUT;
+    config.pull_up_en = GPIO_PULLUP_ENABLE;
+    config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    config.intr_type = GPIO_INTR_DISABLE;
+
+    gpio_config(&config);
+
     // loop
     while(1) {
-        gpio_set_level(LED_PIN, 1);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        gpio_set_level(LED_PIN, 0);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        if (gpio_get_level(PULSADOR)) 
+            gpio_set_level(LED_PIN, 0);
+        else
+            gpio_set_level(LED_PIN, 1);
     }
 }
